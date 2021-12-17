9bedc55eaba64de265941c1279e481646648a943
강동훈B/SGS DevCamp <dhungang@smilegate.com>
2021-12-17 13:36:09 +0900

    [gw] sign-in API 수정(응답 본문에 토큰)

--- gateway-server/src/main/kotlin/me/donghun/gatewayserver/account/AccountController.kt
+++ gateway-server/src/main/kotlin/me/donghun/gatewayserver/account/AccountController.kt
@@ -1,9 +1,14 @@
 package me.donghun.gatewayserver.account
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.fasterxml.jackson.databind.ObjectMapper
+import me.donghun.gatewayserver.TokenManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class AccountController @Autowired constructor(val accountRepository: AccountRepository) {
@@ -17,12 +22,19 @@ class AccountController @Autowired constructor(val accountRepository: AccountRep
             return ResponseEntity.badRequest().body("username '${account.username}' already exists")
         }
 
+        // TODO password encryption
         accountRepository.save(account)
-        // TODO generate token
-        return ResponseEntity.ok()
-            .header("access-token", "access-token")
-            .header("refresh-token", "refresh-token")
-            .build()
+
+        val accessToken = TokenManager.generateAccessToken(account.username)
+        val refreshToken = TokenManager.generateRefreshToken(account.username)
+
+        return ResponseEntity.ok().body(
+            ObjectMapper().writeValueAsString(
+                hashMapOf("access-token" to accessToken, "refresh-token" to refreshToken)
+            )
+        )
     }
+
+
 
 }
