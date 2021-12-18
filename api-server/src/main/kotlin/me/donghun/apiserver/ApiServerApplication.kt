6e58e92d5b49dc72c4745dd3acbebb967658244a
강동훈B/SGS DevCamp <dhungang@smilegate.com>
2021-12-18 19:59:09 +0900

    샘플 유저 추가

--- api-server/src/main/kotlin/me/donghun/apiserver/ApiServerApplication.kt
+++ api-server/src/main/kotlin/me/donghun/apiserver/ApiServerApplication.kt
@@ -1,10 +1,28 @@
 package me.donghun.apiserver
 
+import me.donghun.apiserver.account.Account
+import me.donghun.apiserver.account.AccountRepository
+import me.donghun.apiserver.account.UserRole
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ApiServerApplication
+class ApiServerApplication {
+
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
+
+    @Bean
+    fun runner(): ApplicationRunner {
+        return ApplicationRunner {
+            accountRepository.save(Account(null, "admin", "admin", listOf(UserRole.ADMIN)))
+            accountRepository.save(Account(null, "user", "user", listOf(UserRole.NORMAL)))
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ApiServerApplication>(*args)
