d8fc24ba56c82a97624c312142d17677f5c4480c
강동훈B/SGS DevCamp <dhungang@smilegate.com>
2021-12-19 11:25:05 +0900

    [gw] 각 API에 대해 라우팅 설정

--- gateway-server/src/main/kotlin/me/donghun/gatewayserver/GatewayServerApplication.kt
+++ gateway-server/src/main/kotlin/me/donghun/gatewayserver/GatewayServerApplication.kt
@@ -5,16 +5,39 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
+@Controller
 class GatewayServerApplication {
+    val apiServer = "http://localhost:8080/"
+
     @Bean
-    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator {
+    fun routeLocator(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes()
             .route {
-                it.path("/hi").filters {
-                    f -> f.addRequestHeader("Hello", "World")
-                }.uri("http://localhost:8080/hello")
+                it.path("/signin").uri(apiServer)
+            }
+            .route {
+                it.path("/signup").uri(apiServer)
+            }
+
+            .route {
+                it.method(HttpMethod.GET)
+                    .and()
+                    .path("/menus").filters {
+                        f -> f.filter(AuthFilter().apply(UserRole.USER))
+                }.uri(apiServer)
+            }
+
+            .route {
+                it.method(HttpMethod.POST)
+                    .and()
+                    .path("/menus").filters {
+                        f -> f.filter(AuthFilter().apply(UserRole.OWNER))
+                }.uri(apiServer)
             }
             .build()
     }
