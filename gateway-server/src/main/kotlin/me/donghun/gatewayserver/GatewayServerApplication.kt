10c93afd861b3206fcb307d79cc7e39394cd94d2
강동훈B/SGS DevCamp <dhungang@smilegate.com>
2021-12-20 17:17:17 +0900

    [gw] CORS 설정 추가(Configuration)

--- gateway-server/src/main/kotlin/me/donghun/gatewayserver/GatewayServerApplication.kt
+++ gateway-server/src/main/kotlin/me/donghun/gatewayserver/GatewayServerApplication.kt
@@ -5,9 +5,14 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @SpringBootApplication
 @Controller
@@ -49,4 +54,23 @@ class GatewayServerApplication {
 
 fun main(args: Array<String>) {
     runApplication<GatewayServerApplication>(*args)
+}
+
+@Configuration
+class PreFlightCorsConfiguration {
+    @Bean
+    fun corsFilter(): CorsWebFilter {
+        return CorsWebFilter(corsConfigurationSource())
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration().applyPermitDefaultValues()
+        config.addAllowedMethod(HttpMethod.POST)
+        config.addAllowedMethod(HttpMethod.GET)
+        config.addAllowedMethod(HttpMethod.OPTIONS)
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
 }
